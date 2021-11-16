acce5409b8afb2f99fa15f35a1c1355109536388
BarrySlyDelgado <barryjsly@gmail.com>
2021-11-16 14:08:37 -0500

    client to client communications

--- src/Chat.py
+++ src/Chat.py
@@ -1,11 +1,45 @@
 import ChatClient
 import argparse
+import time
+import select
+import sys
+
+def start_chat(name):
+
+	client = ChatClient.ChatClient(name)
+	start_time = time.time()
+	while(True):
+		client.check_sockets()
+		client.check_catalog()
+		current_time = time.time()
+		t = current_time - start_time
+		if t > 60:
+			client.update_catalog()
+			start_time = time.time()
 
 
-def start_chat(name)
+		print('''Select Option: \n 1: show current users 
+					\n 2: show current connections 
+					\n 3: connect to a user
+					\n 4: send message to a user''')
 
-	client = ChatClient(name)
-	
+		i, o, e = select.select( [sys.stdin], [], [], 5 )
+		if i:
+			choice = sys.stdin.readline().strip()
+			if choice == '1':
+				client.show_current_users()
+			elif choice == '2':
+				client.show_current_connections()
+			elif choice == '3':
+				print('Enter the user you would like to connect to:')
+				user = raw_input()
+				client.request_DM(user)
+			elif choice == '4':	
+				print('Enter the user you would like to message:')
+				user = raw_input()	
+				print('Enter your message to {}:'.format(user))
+				message = raw_input()
+				client.send_DM(user, message)
 
 
 
--- src/ChatClient.py
+++ src/ChatClient.py
@@ -10,63 +10,134 @@ class ChatClient(object):
 		self.name = name
 		self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		self.socket.bind((socket.gethostname(), 0))
-		serversocket.listen(5)
-		self.socket_list = {"Master": self.socket}
+		self.socket.listen(5)
+		self.socket_list = {self.socket}
 		self.user_list = {}
+		self.accepted_connections = {}
 		self.update_catalog()
-		self.check_catalog()
 		
+	def show_current_users(self):
+		for user in self.user_list:
+			print(user)
 
-	def update_catalog(self)
-		data = {"type" : "nsbs-chat", "owner" : name, "port" : self.socket.getsockname()[1]}
+	def show_current_connections(self):
+		for user in self.accepted_connections:
+			print(user)
+
+	def update_catalog(self):
+		data = {"type" : "nsbs-user", "owner" : self.name, "port" : self.socket.getsockname()[1]}
 		catalog_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 		data = json.dumps(data)
 		data = data.encode('utf_8')
 		catalog_socket.sendto(data, ('catalog.cse.nd.edu', 9097))
 
 	def check_catalog(self):
+		print('Looking for users....')
 		subprocess.check_call(['curl', 'http://catalog.cse.nd.edu:9097/query.json', '--output', 'catalog'])    
 		f = open('catalog')                                                                                   
 		data = json.load(f)
-		users  = [x  for x in data if 'type' in x and x['type'] == 'nsbs-chat']
+		users  = [x for x in data if 'type' in x and (x['type'] == 'nsbs-user' or x['type'] == 'nsbs-group')]
 		for user in users:
 			name = user['owner']
 			port = user['port']
 			server = user['name']
-			self.user_list[name] = {'server':server, 'port':port}         
+			node_type = user['type']
+			self.user_list[name] = {'server':server, 'port':port, 'type':node_type}         
 
-	def send_msg(self, user):
-		
-		
+	def send_DM(self, user, message):
+		if user in self.accepted_connections:
+			msg = {'method':'DM', 'name':self.name, 'message':str(message)}
+			msg = json.dumps(msg)
+			msg = msg.encode('utf_8')
+			header = '{}:'.format(len(msg))
+			msg = header.encode('utf_8') + msg
+			self.accepted_connections[user].sendall(msg)
+		else:
+			print('Unable to send message. User {} is either unknown or has not accepted request.'. format(user))
+
+	def request_DM(self, user):	
+		if user in self.user_list:
+			msg = {'method':'Request', 'name':self.name}
+			msg = json.dumps(msg)
+			msg = msg.encode('utf_8')
+			header = '{}:'.format(len(msg))
+			msg = header.encode('utf_8') + msg
+			server = self.user_list[user]['server']
+			port = self.user_list[user]['port']
+			req_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			try:
+				print('connecting to {} on server {} on port {}'.format(user, server, port))
+				req_socket.connect((server, port))
+				req_socket.sendall(msg)
+				self.socket_list.add(req_socket)
+				print('Sent request to {}'.format(user))
+			except socket.error:
+				print('Unable to Connect to user {}'.format(user))	
+		else:
+			print('User {} is unknown.'.format(user))
+			
 	
-	def handle_msg(self, msg):
-		method = msg['msg']
+	def handle_msg(self, msg, socket):
+		method = msg['method']
 		name = msg['name']
-		if method == 'DM':
-			message = msg['msg']
-			print('DM From {}: {}'.format(name, message))
-			
-			
+		if method == 'Request':
+			print('DM Request From {}...'.format(name))
+			print('accept DM request from {}? [y/n]...'.format(name))
+			choice = raw_input()
+			if choice == 'n':
+				return_msg = {'method':'Response', 'name':self.name, 'Response':'n'}
+				return_msg = json.dumps(return_msg)
+				return_msg = return_msg.encode('utf_8')
+				header = '{}:'.format(len(return_msg))
+				return_msg = header.encode('utf_8') + return_msg
+				socket.sendall(return_msg)
+				self.socket_list.remove(socket)
+			elif choice == 'y':
+				self.accepted_connections[name] = socket
+				return_msg = {'method':'Response', 'name':self.name, 'Response':'y'}
+				return_msg = json.dumps(return_msg)
+				return_msg = return_msg.encode('utf_8')
+				header = '{}:'.format(len(return_msg))
+				return_msg = header.encode('utf_8') + return_msg
+				socket.sendall(return_msg)
+
+		elif method == 'Response':
+			response = msg['Response']
+			if response == 'y':
+				print('Messging Request from {} has been ACCEPTED.'.format(name))
+				self.accepted_connections[name] = socket
+			elif response == 'n':
+				print('Messaging Request from {} has been DECLINED.'.format(name))
+				self.socket_list.remove(socket)
+ 
+		elif method == 'DM':
+			x = [x for x in self.accepted_connections if self.accepted_connections[x] == socket]
+			if not x:
+				print('Message from {} has been declined as they have not been accepted'.format(user))
+
+			else:	
+				message = msg['message']
+				print('DM From {}: {}'.format(name, message))
+		
+							
 	def check_sockets(self):
-		readable, writable, exceptional = select.select(socket_list.values(), [], socket_list.values()
+		print('checking open sockets...')
+		readable, writable, exceptional = select.select(list(self.socket_list), [], list(self.socket_list), 2)
 		for read_socket in readable:                                                                  
-			if read_socket == socket_list['Master']:
-				(clientsocket, address) = serversocket.accept()
-				sockets[str(address)] = clientsocket
+			if read_socket == self.socket:
+				print('accepting incomming connection...')
+				(clientsocket, address) = read_socket.accept()
+				self.socket_list.add(clientsocket)
 			else:
 				clientsocket = read_socket
-				address = ''
-				for x in sockets:
-					if sockets[x] == clientsocket:
-						address = x
+
 				header = b''
 				closed = False
-
 				while ':' not in header.decode('utf_8'):
 					msg = clientsocket.recv(1)
 					if not msg:
-						print('client closed removing socket detail for address', address)
-						del sockets[address]
+						print('A Socket has been closed...')
+						self.socket_list.remove(clientsocket)
 						closed = True
 						break
                                         header += msg
@@ -80,11 +151,5 @@ class ChatClient(object):
 						size -= x
 						msg += rsp.decode('utf_8')
 					msg = json.loads(msg)
-					handle_msg(msg)
-			#		return_msg = handle_msg(msg)
-			#		return_msg = json.dumps(return_msg)
-			#		return_msg = return_msg.encode('utf_8')
-			#		header = '{}:'.format(len(return_msg))
-			#		return_msg = header.encode('utf_8') + return_msg
-			#		clientsocket.sendall(return_msg)
+					self.handle_msg(msg, clientsocket)
 
